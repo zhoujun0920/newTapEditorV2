477c67f1ea6da5932b4575ad667d52835655fc9f
zhoujun0920 <zhoujun199009@hotmail.com>
2015-08-14 20:05:03 -0500

    first commit

---
+++ newTabEditor/Chord.swift
@@ -0,0 +1,9 @@
+//
+//  Chord.swift
+//  newTabEditor
+//
+//  Created by Jun Zhou on 8/10/15.
+//  Copyright (c) 2015 Jun Zhou. All rights reserved.
+//
+
+import Foundation
---
+++ newTabEditor/FretsBoard.swift
@@ -0,0 +1,9 @@
+//
+//  FretsBoard.swift
+//  newTabEditor
+//
+//  Created by Jun Zhou on 8/14/15.
+//  Copyright (c) 2015 Jun Zhou. All rights reserved.
+//
+
+import Foundation
